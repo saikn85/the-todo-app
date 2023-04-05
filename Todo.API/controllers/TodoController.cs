676c8e10699d80514bcde3fbdacc72ea1188d4f3
Mad Max <85madmaxfury@gmail.com>
2023-04-05 23:18:24 +0530

    base skeleton for controllers

---
+++ Todo.API/controllers/TodoController.cs
@@ -0,0 +1,41 @@
+using Todo.Domain;
+
+[Route("[controller]")]
+public class TodoController : ControllerBase
+{
+    private readonly ILogger<TodoController> _log;
+    public TodoController(ILogger<TodoController> log)
+    {
+        _log = log;
+    }
+
+    [HttpGet]
+    public IActionResult GetAsync()
+    {
+
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetAsync([FromRoute] int id)
+    {
+
+    }
+
+    [HttpPost]
+    public IActionResult PostAsync([FromBody] Todo todo)
+    {
+
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult PatchAsync([FromRoute] int id, [FromBody] Todo todo)
+    {
+
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteAsync([FromRoute] int id)
+    {
+
+    }
+}
