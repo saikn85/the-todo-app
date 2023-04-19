191dcdef48659c6b0d440e2f8b035a6b27e20c65
saikn85 <85madmaxfury@gmail.com>
2023-04-19 11:39:43 +0530

    initial commit

---
+++ OneHasToDoThings.DB/dbo/Tables/dbo.TaskType.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TaskType] (
+    [TaskTypeId]      SMALLINT     IDENTITY (1, 1) NOT NULL,
+    [TaskDescription] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_TaskType_TaskTypeId] PRIMARY KEY CLUSTERED ([TaskTypeId] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UX_TaskType_NC]
+    ON [dbo].[TaskType]([TaskDescription] ASC);
+
