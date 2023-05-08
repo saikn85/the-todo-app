eb3016803cf4fb45a45e3a9a57a2ea2664911119
saikn85 <85madmaxfury@gmail.com>
2023-05-08 10:29:52 +0530

    added repository

--- OneHasToDoThings.App/OneHasToDoThings.Services/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OneHasToDoThings.Services
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ OneHasToDoThings.App/OneHasToDoThings.Services/ITodoRepository.cs
@@ -0,0 +1,13 @@
+﻿using OneHasToDoThings.Domain;
+
+namespace OneHasToDoThings.AppCore
+{
+    public interface ITodoRepository : IDisposable
+    {
+        Task<ToDo> CreateAsync(ToDo todo);
+        Task<bool> UpdateAsync(ToDo todo);
+        Task<bool> DeleteAsync(ToDo todo);
+        Task<IEnumerable<ToDo>> ReadAsync(TaskType? tType, DateTime? tCreated);
+        Task<ToDo> ReadAsync(Guid id, string? tName, string? tDesc);
+    }
+}
