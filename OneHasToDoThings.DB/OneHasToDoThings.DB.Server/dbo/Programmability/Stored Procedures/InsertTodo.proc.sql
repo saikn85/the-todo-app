19a18e3542ac2c14ac90378d165da3d5616fa0ca
saikn85 <85madmaxfury@gmail.com>
2023-05-21 19:33:57 +0530

    updated db and added UT

---
+++ OneHasToDoThings.DB/OneHasToDoThings.DB.Server/dbo/Programmability/Stored Procedures/InsertTodo.proc.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[InsertToDo]
+(
+    @TaskName VARCHAR(20),
+    @TaskDescription VARCHAR(MAX),
+    @TaskTypeId SMALLINT
+)
+AS
+    BEGIN
+        INSERT INTO [dbo].[Todo] 
+            (
+                TaskName, 
+                TaskDescription, 
+                TaskTypeId
+            ) 
+        VALUES
+            (
+                @TaskName,
+                @TaskDescription,
+                @TaskTypeId
+            )
+    END;
+GO
