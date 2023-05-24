4ae6da25d07685d38ece8ca09093b5beb316b2b0
saikn85 <85madmaxfury@gmail.com>
2023-05-24 10:34:11 +0530

    learning react

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+    // JSX Extension enables us to write HTML templated code
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
