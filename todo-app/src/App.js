0d850647eb393b779429d7ac790e07293a6272cf
saikn85 <85madmaxfury@gmail.com>
2023-05-26 10:13:34 +0530

    change tab size

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,26 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return (
-    // JSX Extension enables us to write HTML templated code - Babel.js
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		// JSX Extension enables us to write HTML templated code - Babel.js
+		<div className="App">
+			<header className="App-header">
+				<img
+					src={logo}
+					className="App-logo"
+					alt="logo"
+				/>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer">
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
